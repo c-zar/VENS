2ac4865cc330171d1bb45ff5b972b690321899b0
Czar Parreno <czar.parreno@ukg.com>
2021-11-04 19:45:30 -0400

    Updates

---
+++ app/pages/index.svelte-7599221d.js
@@ -0,0 +1,37 @@
+import{S as H,i as M,s as $,j as J,m as K,o as L,x as _,u as A,v as Q,e as f,t as k,k as x,c as p,a as g,g as j,d as m,n as T,b as G,f as U,D as i,w as X,F as Y,r as Z}from"../chunks/vendor-2ff90f39.js";/* empty css                                                   */import{P as ee}from"../chunks/PostPreview-a9afc70b.js";import{g as te,a as re}from"../chunks/get-posts-e96e0dff.js";import"../chunks/paths-28a87002.js";function q(l,r,n){const e=l.slice();return e[2]=r[n],e}function B(l){let r,n;return r=new ee({props:{post:l[2]}}),{c(){J(r.$$.fragment)},l(e){K(r.$$.fragment,e)},m(e,c){L(r,e,c),n=!0},p(e,c){const h={};c&1&&(h.post=e[2]),r.$set(h)},i(e){n||(_(r.$$.fragment,e),n=!0)},o(e){A(r.$$.fragment,e),n=!1},d(e){Q(r,e)}}}function oe(l){let r,n,e,c,h,F,w,W,S,v,V,N,y,P,E,b,u=l[0],o=[];for(let a=0;a<u.length;a+=1)o[a]=B(q(l,u,a));const R=a=>A(o[a],1,1,()=>{o[a]=null});return{c(){r=f("div"),n=f("div"),e=f("div"),c=f("h1"),h=k("What is Ven Studios?"),F=x(),w=f("p"),W=k(`At Ven Studio, we are convinced that the digital space is an invaluable tool for bringing\r
+        transparency and equity to the art market. We partner with artists, developers and creative\r
+        managers to release their NFT art onto the blockchain. Our team is working around the clock,\r
+        committing their energy to promoting Ven Studio Collections in different platform to\r
+        maximize traffic and increase audience. Our artist come from 3rd world countries and we\r
+        collaborate with them along with our software engineers and programmers to promote their\r
+        underrated art onto the blockchain. In showcasing Their Collections around the world, we\r
+        also hope to allow art lovers and collectors alike to explore new artistic horizons, to\r
+        embrace new cultures and to be inspired by the works of talented artists that they never\r
+        would have come across if it wasn't for Ven Studios.`),S=x(),v=f("p"),V=k(`The NFT market, like many tech-centric areas, has traditionally been dominated by men, and a\r
+        women-led project was a much-welcomed change. While the rise of blockchain-based digital\r
+        artwork is notable, many of the artists responsible for creating today\u2019s most sought after\r
+        NFTs are men. While the ratio of male to female NFT artists remains unclear, statistics show\r
+        that there are considerably less women than men involved in the overall crypto sector. A\r
+        recent survey conducted by cryptocurrency exchange Gemini found that just 26% of females\r
+        hold crypto. We are a proud Female-led NFT Studio. With out team consisting of 80% women in\r
+        the field of Technology, Fine Arts and Management. Our goal is to increase female engagement\r
+        in crypto`),N=x(),y=f("p"),P=k(`\u201CWomen belong in all places where decisions are being made. It shouldn\u2019t be that women are\r
+        the exception\u201D \u2014 Ruth Bader-Ginsburg.`),E=x();for(let a=0;a<o.length;a+=1)o[a].c();this.h()},l(a){r=p(a,"DIV",{class:!0});var s=g(r);n=p(s,"DIV",{class:!0});var t=g(n);e=p(t,"DIV",{});var d=g(e);c=p(d,"H1",{});var O=g(c);h=j(O,"What is Ven Studios?"),O.forEach(m),F=T(d),w=p(d,"P",{});var C=g(w);W=j(C,`At Ven Studio, we are convinced that the digital space is an invaluable tool for bringing\r
+        transparency and equity to the art market. We partner with artists, developers and creative\r
+        managers to release their NFT art onto the blockchain. Our team is working around the clock,\r
+        committing their energy to promoting Ven Studio Collections in different platform to\r
+        maximize traffic and increase audience. Our artist come from 3rd world countries and we\r
+        collaborate with them along with our software engineers and programmers to promote their\r
+        underrated art onto the blockchain. In showcasing Their Collections around the world, we\r
+        also hope to allow art lovers and collectors alike to explore new artistic horizons, to\r
+        embrace new cultures and to be inspired by the works of talented artists that they never\r
+        would have come across if it wasn't for Ven Studios.`),C.forEach(m),S=T(d),v=p(d,"P",{});var z=g(v);V=j(z,`The NFT market, like many tech-centric areas, has traditionally been dominated by men, and a\r
+        women-led project was a much-welcomed change. While the rise of blockchain-based digital\r
+        artwork is notable, many of the artists responsible for creating today\u2019s most sought after\r
+        NFTs are men. While the ratio of male to female NFT artists remains unclear, statistics show\r
+        that there are considerably less women than men involved in the overall crypto sector. A\r
+        recent survey conducted by cryptocurrency exchange Gemini found that just 26% of females\r
+        hold crypto. We are a proud Female-led NFT Studio. With out team consisting of 80% women in\r
+        the field of Technology, Fine Arts and Management. Our goal is to increase female engagement\r
+        in crypto`),z.forEach(m),N=T(d),y=p(d,"P",{});var D=g(y);P=j(D,`\u201CWomen belong in all places where decisions are being made. It shouldn\u2019t be that women are\r
+        the exception\u201D \u2014 Ruth Bader-Ginsburg.`),D.forEach(m),d.forEach(m),E=T(t);for(let I=0;I<o.length;I+=1)o[I].l(t);t.forEach(m),s.forEach(m),this.h()},h(){G(n,"class","flex-grow divide-y divide-gray-300 dark:divide-gray-700 p-10"),G(r,"class","flex flex-col flex-grow px-4 max-w-4xl mx-auto")},m(a,s){U(a,r,s),i(r,n),i(n,e),i(e,c),i(c,h),i(e,F),i(e,w),i(w,W),i(e,S),i(e,v),i(v,V),i(e,N),i(e,y),i(y,P),i(n,E);for(let t=0;t<o.length;t+=1)o[t].m(n,null);b=!0},p(a,[s]){if(s&1){u=a[0];let t;for(t=0;t<u.length;t+=1){const d=q(a,u,t);o[t]?(o[t].p(d,s),_(o[t],1)):(o[t]=B(d),o[t].c(),_(o[t],1),o[t].m(n,null))}for(Z(),t=u.length;t<o.length;t+=1)R(t);X()}},i(a){if(!b){for(let s=0;s<u.length;s+=1)_(o[s]);b=!0}},o(a){o=o.filter(Boolean);for(let s=0;s<o.length;s+=1)A(o[s]);b=!1},d(a){a&&m(r),Y(o,a)}}}const he=!0,de=async()=>({props:{posts:te().map(l=>l.metadata),projects:re().map(l=>l.metadata)}});function ae(l,r,n){let{posts:e}=r,{projects:c}=r;return l.$$set=h=>{"posts"in h&&n(0,e=h.posts),"projects"in h&&n(1,c=h.projects)},[e,c]}class me extends H{constructor(r){super();M(this,r,ae,oe,$,{posts:0,projects:1})}}export{me as default,de as load,he as prerender};
